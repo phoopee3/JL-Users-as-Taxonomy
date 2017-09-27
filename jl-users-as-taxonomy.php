70ab496897d55b77b73c154e422f0afe891687a6
Jason Lawton <lawton.jason@gmail.com>
2017-09-26 20:26:01 -0400

    getting started

---
+++ jl-users-as-taxonomy.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Users as Taxonomy
+ * Plugin URI: https://jasonlawton.com
+ * Description: Create a taxonomy out of the user list
+ * Version: 1.0.0
+ * Author: Jason Lawton
+ * Author URI: https://jasonlawton.com
+ * License: MIT
+ */
+ 
